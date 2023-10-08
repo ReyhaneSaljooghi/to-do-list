8d441f0fe7255110716c3636add59f2f1dc4e8b9
reyhane <reysalphimo@gmail.com>
2023-10-08 21:57:42 +0330

    commit-sec

--- src/component.jsx
+++ src/component.jsx
@@ -1,29 +1,51 @@
 import { findByLabelText } from '@testing-library/react';
-import React from 'react';
-import { useState } from 'react';
+import React, { useRef } from 'react';
+import { useState, useEffect } from 'react';
 import Plusimg from "./Plus.png";
 import Check from "./Check.png";
+import Delete from "./Delete.png";
 
 const MainDiv = () => {
   
  
   const [newtask,setnewTask]=useState("  add new task");
-  const[tasks,settasks]=useState([]);
+  const inputeRef =useRef(null);
+  const date = new Date();
+    const showTime = date.getHours()
+        + ':'+date.getMinutes() +'  -  '+ date.toLocaleDateString('en-En',{  month: 'short', day: 'numeric' });
+  const[tasks,settasks]=useState(()=>{
+    const data =localStorage.getItem("ITEMS")
+    if (data==null) return []
+    return JSON.parse(data)
+  });
+  useEffect (()=>{
+    localStorage.setItem("ITEMS",JSON.stringify(tasks))
+  },[tasks])
   function toggle(id){
+    console.log(id)
     settasks(currenttasks =>
       currenttasks?.map(task =>{
         if(task.id==id){
-        task.completed=!task.completed
+          console.log('hello')
+       return {...task,completed:!task.completed}
         }
+        else
+        return task
       })
     )
   }
-
+  console.log(tasks)
   
-  function handleSubmit(e){
-    settasks(currtasks=>[...currtasks, {id: crypto.randomUUID(),title: newtask, completed:false}])
+  function handleSubmit(title){
+    console.log(inputeRef.current.value)
+    settasks(currtasks=>[ {id: crypto.randomUUID(),title, completed:false, showTime},...currtasks])
+    inputeRef.current.value=""
   }
-  
+  function del(id){
+    settasks(currenttasks=>{
+      return currenttasks.filter(task => task.id!=id)
+    })
+  }
     const styles = {
         maindiv: {
             fontSize: "18px",
@@ -33,105 +55,133 @@ const MainDiv = () => {
             boxShadow: "1px 16px 88px -4px rgba(0,0,0,0.78)",           
             width: "55%",
             height: "88%",
-            borderRadius: "1%",
+            borderRadius: "6%",
+            overflow:"hidden",
             display:"flex",
             flexDirection:"column",
-            justifyContent:"space-between",
-
-          },
-          title: {
-            color:"#201859",
-            opacity:"80%"
-          },
-          date: {
-              opacity: "50%",
-              display: "flex",
-              justifyContent: "center",
-              marginTop: "28px"
-          },
-          line: {
-            borderBottom: "1px solid black",
-            opacity: "25%",
-            padding: "0",
-            margin: "0",
-            width: "100%"
-          },
-          plusButton: {
-            borderRadius: "100%",
-            backgroundColor: "#7379EA",
-            height: "48px",
-            width: "48px",
-            border: "0px",
-            margin:"0px",
-            marginTop:"-10px",
-            boxShadow: " 1px 13px 18px -4px rgba(115,121,234,0.78)",
-            display:"flex",
-            justifyContent:"center",
-            alignItems:"center"
-          },
-          input:{
-            height: "40px",
-            width: "80%",
-            backgroundColor:"lightgray",
-            border:"0px",
-            margin:"0px",
-            padding:"0px",
-            borderRadius: '5px', 
-            overflow: 'hidden',
+        },
+            title: {
+              color:"#201859",
+              opacity:"80%",
             
-
-          },
-          addDiv:{
-            display:"flex",
-            flexDirection:"column",
-            justifyContent:"end",
-            alignItems:"center",
-            marginBottom:"10px"
-          },
-          task:{
-            backgroundColor:"rgb(115, 121, 234, 0.2)",
-            marginBottom:"5px",
-            height:"45px",
-            width:"85%",
-            borderRadius: '5px', 
-            overflow: 'hidden',
-            display:"flex",
-            flexDirection:"row",
-            justifyContent:"space-between",
-            padding:"5px",
-            paddingTop:"18px",
-            color:"rgba( 32, 24,89, 0.7)"
+            },
+            date: {
+                opacity: "50%",
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "28px",
+              
+            },
+            line: {
+              borderBottom: "1px solid #201859",
+              opacity: "40%",
+              padding: "0",
+              margin: "0",
+              width: "100%"
+            },
+           
+            input:{
+              height: "40px",
+              width: "80%",
+              backgroundColor:"whiteSmoke",
+              border:"0px",
+              margin:"0px",
+              padding:"0px",
+              borderRadius: '10px', 
+              overflow: 'hidden',
+              borderColor:"#7379EA",
+              borderWidth: "1",
+              border: "3px solid rgb(115,121,234,0.4)",
+              paddingLeft:"10px",
+              
+            },
+            addDiv:{
+              display:"flex",
+              flexDirection:"column",
+              justifyContent:"end",
+              alignItems:"center",
+              marginBottom:"10px"
+            },
+         
           
-          },
-          checkbox:{
-        
-            height: "20px",
-            width: "20px",
-            backgroundColor:"white",
-            border:"0px"
+            checkimg:{
+              marginRight:"50px",
+            },
+            delete:{
+          
+              height: "26px",
+              width: "26px",
+              backgroundColor:"rgb(115, 121, 234, 0.005)",
+              border:"0px",
+              borderRadius: '20px', 
+              overflow: 'hidden',
+              // boxShadow: "-2px 6px 20px -2px rgba(115,121,234,0.75)"
+  
+            },
+            alltask:{
+              overflowY:"scroll",
+              height:"60%",
+              display:"flex",
+              flexDirection:"column",
+             
+              
+            },
+         
+            secdiv:{
+              height:"80%"
+            },
+            towbotton:{
+    
+              display:"flex",
+             
+            },
+            checkimg:{
+             marginRight:"28px",
+             position:"absolute",
+             left:"28%",
+            },
+  
 
-          }
-
+          
+          
+         
+          // towbotton:{
+          //   display:"flex",
+          // }
     };
     return(
      <div style={styles.maindiv}>
-      <div>
+      <div style={styles.secdiv}>
          <h2 style={styles.title}>My Tasks</h2>
          <h1></h1> <div style={styles.line}></div>
          <div style={styles.date} >{new Date().toLocaleDateString('en-En',{ year: 'numeric', month: 'long', day: 'numeric' })}</div>
-         <ul>
+         <ul style={styles.alltask}>
           {tasks.map(task=>{
             return(
-              <div style={styles.task} key={task.id}>
-                <label>
-                {task.title}
-                </label>
-                <button style={styles.checkbox} onClick={task=>toggle(task.id)}>
+              <div className='task' key={task.id}>
+                <div className='taskrow'>
+                <div className='checkandtilte'>
+                <button className='checkbox'type='submit' onClick={()=>toggle(task.id)}>
                   {
-                    !task.completed?(<div></div>):( <img width="15px" height="15px"src={Check}/>)
+                    !task.completed?(<div></div>):( <img style={styles.checkimg} width="9px"  height="9px"src={Check}/>)
                   }
             
                 </button>
+                <label>
+                {task.title}
+                </label>
+                </div>
+            
+                <button style={styles.delete}  onClick={()=>del(task.id)} >
+                  {
+                  ( <img  width="11px" height="14.5x"src={Delete}/>)
+                  }
+            
+                </button>
+            
+                </div>
+                <p className='task-time' align="center"> {task.showTime}</p>
+                
                 {/* <input style={styles.checkbox} type="checkbox" /> */}
               </div>
             );
@@ -139,8 +189,8 @@ const MainDiv = () => {
          </ul>
       </div>
      <div style={styles.addDiv}>
-          <input style={styles.input} value={newtask}  type="text" onChange={e => setnewTask(e.target.value)}/>
-          <button type='submit'style={styles.plusButton} onClick={newtask=>handleSubmit(newtask)}><img width="15px" height="15px"src={Plusimg}/></button>
+          <input ref={inputeRef} placeholder="add a task" style={styles.input}  type="text" />
+          <button type='submit'className='plusButton' onClick={newtask=>handleSubmit(inputeRef.current.value)}><img width="15px" height="15px"src={Plusimg}/></button>
      </div>
   
      </div>
